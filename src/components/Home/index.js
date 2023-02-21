f807e386633d625edd3536e41434ee52b2160d16
usama-zafar-codegenio <usama.zafar@codegenio.com>
2023-02-22 01:36:59 +0400

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ import './index.scss'
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const nameArray = ['U','s', 'a', 'm', 'a']
   const jobArray = [
     'w',
     'e',
@@ -42,11 +42,12 @@ const Home = () => {
             <span className={`${letterClass} _12`}>i,</span>
             <br />
             <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img
+            <span className={`${letterClass} _14`}>'m &nbsp;</span> 
+            {/* <img
               src={LogoTitle}
               alt="JavaScript Developer Name, Web Developer Name"
-            />
+            /> */}
+
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
@@ -59,7 +60,7 @@ const Home = () => {
               idx={22}
             />
           </h1>
-          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <h2>Front End Developer / JavaScript Developer</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,76 +1,76 @@
-import React, { useEffect, useState } from "react";
-import Loader from "react-loaders";
-import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss";
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase';
+// import React, { useEffect, useState } from "react";
+// import Loader from "react-loaders";
+// import AnimatedLetters from "../AnimatedLetters";
+// import "./index.scss";
+// import { getDocs, collection } from 'firebase/firestore';
+// import { db } from '../../firebase';
 
-const Portfolio = () => { 
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const [portfolio, setPortfolio] = useState([]);
+// const Portfolio = () => { 
+//     const [letterClass, setLetterClass] = useState('text-animate');
+//     const [portfolio, setPortfolio] = useState([]);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+//     useEffect(() => {
+//         const timer = setTimeout(() => {
+//             setLetterClass('text-animate-hover');
+//         }, 3000);
 
-        return () => {
-            clearTimeout(timer);
-        }
-    });
+//         return () => {
+//             clearTimeout(timer);
+//         }
+//     });
 
-    useEffect(() => {
-        getPortfolio();
-    }, []);
+//     useEffect(() => {
+//         getPortfolio();
+//     }, []);
 
-    const getPortfolio = async () => {
-        const querySnapshot = await getDocs(collection(db, 'portfolio'));
-        setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
-    }
+//     const getPortfolio = async () => {
+//         const querySnapshot = await getDocs(collection(db, 'portfolio'));
+//         setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
+//     }
 
-    const renderPortfolio = (portfolio) => {
-        return (
-            <div className="images-container">
-                {
-                    portfolio.map((port, idx) => {
-                        return (
-                            <div className="image-box" key={idx}>
-                                <img 
-                                src={port.image}
-                                className="portfolio-image"
-                                alt="portfolio" />
-                                <div className="content">
-                                    <p className="title">{port.name}</p>
-                                    <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        );
-    }
+//     const renderPortfolio = (portfolio) => {
+//         return (
+//             <div className="images-container">
+//                 {
+//                     portfolio.map((port, idx) => {
+//                         return (
+//                             <div className="image-box" key={idx}>
+//                                 <img 
+//                                 src={port.image}
+//                                 className="portfolio-image"
+//                                 alt="portfolio" />
+//                                 <div className="content">
+//                                     <p className="title">{port.name}</p>
+//                                     <h4 className="description">{port.description}</h4>
+//                                     <button
+//                                         className="btn"
+//                                         onClick={() => window.open(port.url)}
+//                                     >View</button>
+//                                 </div>
+//                             </div>
+//                         )
+//                     })
+//                 }
+//             </div>
+//         );
+//     }
 
 
-    return (
-        <>
-            <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"Portfolio".split("")}
-                        idx={15}
-                    />
-                </h1>
-                <div>{renderPortfolio(portfolio)}</div>
-            </div>
-            <Loader type="pacman" />
-        </>
-    );
-}
+//     return (
+//         <>
+//             <div className="container portfolio-page">
+//                 <h1 className="page-title">
+//                     <AnimatedLetters
+//                         letterClass={letterClass}
+//                         strArray={"Portfolio".split("")}
+//                         idx={15}
+//                     />
+//                 </h1>
+//                 <div>{renderPortfolio(portfolio)}</div>
+//             </div>
+//             <Loader type="pacman" />
+//         </>
+//     );
+// }
 
-export default Portfolio;
+// export default Portfolio;
